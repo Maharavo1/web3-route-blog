b7370c9c7d383c341b3922afc378036e8f5ec251
Maharavo1 <hei.maharavo@gmail.com>
2024-03-11 11:16:48 +0300

    feat : add list of blog

---
+++ src/listblog/index.tsx
@@ -0,0 +1,35 @@
+export type Blog = {
+    id: string,
+    title: string
+    image: string
+  }
+export const blogMocks: Blog[] = [
+    {
+      id: "BLOG1",
+      title: "TITRES1",
+    
+      image: "https://picsum.photos/180"
+    },
+    {
+      id: "BLOG2",
+      title: "TITRE2",
+      image: "https://picsum.photos/180"
+    },
+    {
+      id: "BLOG3",
+      title: "TITRE3",
+      image: "https://picsum.photos/180"
+    },
+    {
+        id: "BLOG4",
+        title: "TITRE4",
+        image: "https://picsum.photos/180"
+      },
+      {
+        id: "BLOG5",
+        title: "TITRE5",
+        image: "https://picsum.photos/180"
+      },
+    
+  ];
+  
