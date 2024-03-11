a4fdb1a82afe80ef4134f3f744710ae397e842a8
Maharavo1 <hei.maharavo@gmail.com>
2024-03-11 11:58:44 +0300

    feat : update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
 export default nextConfig;
+const nextConfig = {
+    async redirects() {
+        return [
+          {
+            source: '/',
+            destination: '/home',
+            permanent: true,
+          }
+        ]
+      },
+    images: {
+        domains: [
+          "picsum.photos",
+        ],
+      },
+};
