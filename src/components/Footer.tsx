393845145675138fb20e254d344dbbbcd2108ef4
Maharavo1 <hei.maharavo@gmail.com>
2024-03-11 11:14:13 +0300

    feat : add components file

---
+++ src/components/Footer.tsx
@@ -0,0 +1,8 @@
+export function Footer() {
+    return (
+      <footer className="w-full h-[60px] text-white bg-green-800 flex items-center justify-center">
+        Footer
+      </footer>
+    );
+  }
+  
---
+++ src/components/Header.tsx
@@ -0,0 +1,8 @@
+export function Header() {
+    return (
+      <div className="w-full h-[60px] text-white bg-yellow-800 flex items-center justify-center">
+        Header
+      </div>
+    );
+  }
+  
